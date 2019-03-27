93e89b9896afd78a87c3fafa26385bab691c2fc8
Henry Sztanski <henry@sztanski.com>
2019-03-27 14:11:20 +0000

    Done.

--- lib/concerns.rb
+++ lib/concerns.rb
@@ -15,5 +15,17 @@ module Concerns
       obj.save
       obj
     end
+
+    def find_by_name(name)
+      self.all.detect{|obj| obj.name == name}
+    end
+
+    def find_or_create_by_name(name)
+      if (obj = self.find_by_name(name)).nil?
+        self.create(name)
+      else
+        obj
+      end
+    end
   end
 end
