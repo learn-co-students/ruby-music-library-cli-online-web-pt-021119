105ce804bc0d6b8b2798fb4c67ee4dbc52370b7b
Henry Sztanski <henry@sztanski.com>
2019-03-27 12:53:22 +0000

    Done.

--- lib/concerns.rb
+++ lib/concerns.rb
@@ -1,19 +1,9 @@
 module Concerns
   module InstanceMethods
-    def save
-      self.class.all << self
-    end
+
   end
 
   module ClassMethods
-    def destroy_all
-      self.all.clear
-    end
 
-    def create(name)
-      object = self.new(name)
-      self.all << object
-      object
-    end
   end
 end
