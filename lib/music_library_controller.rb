ed195fc3ec175e3e3b96b9ff3f422114630deeb0
Henry Sztanski <henry@sztanski.com>
2019-03-27 19:33:20 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 class MusicLibraryController
-  attr_accessor :path, :importer, :files
+  attr_accessor :path, :importer
 
   def initialize(path = "./db/mp3s")
     @path = path
@@ -11,17 +11,31 @@ class MusicLibraryController
 
   def call
     input = ""
-    while input != "exit"
-      puts "Welcome to your music library!"
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
-      puts "To list all of the genres in your library, enter 'list genres'."
-      puts "To list all of the songs by a particular artist, enter 'list artist'."
-      puts "To list all of the songs of a particular genre, enter 'list genre'."
-      puts "To play a song, enter 'play song'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
-      input = gets.chomp
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    while input = gets.chomp
+      break if input == "exit"
+      case input
+        when "list songs"
+          list_songs
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
+        when "play song"
+          play_song
+      end
     end
   end
 
@@ -29,7 +43,11 @@ class MusicLibraryController
     files = self.importer.files.collect{|file_name| file_name.gsub(".mp3", "")}
     files.sort_by!{|file_name| file_name.split(" - ")[1]}
     files.each.with_index(1) {|file, index| puts "#{index}. #{file}"}
-    @files = files
+  end
+
+  def ordered_files #same as list_songs but does not use puts only for spec purposes
+    files = self.importer.files.collect{|file_name| file_name.gsub(".mp3", "")}
+    files.sort_by!{|file_name| file_name.split(" - ")[1]}
   end
 
   def list_artists
@@ -63,12 +81,11 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     song_number = gets.chomp.to_i
-    song_names = self.list_songs
+    song_names = self.ordered_files #had to create #ordered_files because #list_songs has puts statements which conflict with the test
     if song_number > 0 && song_number <= song_names.size
-      song_name = song_names.split(" - ")[1]
-      artist_name = song_names.split(" - ")[0]
-      "Playing #{song_name} by #{artist_name}"
-      binding.pry
+      song_name = song_names[song_number - 1].split(" - ")[1]
+      artist_name = song_names[song_number - 1].split(" - ")[0]
+      puts "Playing #{song_name} by #{artist_name}"
     end
   end
 end
