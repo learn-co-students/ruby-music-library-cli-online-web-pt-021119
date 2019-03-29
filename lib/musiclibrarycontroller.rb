6ab5691bb1ff93d96d06f27aba680bfb183b74a7
Cody Yantis <cyantis@gmail.com>
2019-03-29 02:06:16 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -1,5 +1,6 @@
-class MusicLibraryController
+require 'pry'
 
+class MusicLibraryController
   def initialize(path = "./db/mp3s")
     MusicImporter.new(path).tap {|i| i.import}
   end
@@ -19,6 +20,39 @@ class MusicLibraryController
       puts "What would you like to do?"
 
       input = gets.downcase.chomp
+    end
+  end
+
+  def list_songs
+    sorted = Song.all.sort_by {|s| s.name}
+    sorted.each.with_index(1) {|s, i| puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"}
+  end
+
+  def list_artists
+    sorted = Artist.all.sort_by {|s| s.name}
+    sorted.each.with_index(1) {|s, i| puts "#{i}. #{s.name}"}
+  end
+
+  def list_genres
+    sorted = Genre.all.sort_by {|s| s.name}
+    sorted.each.with_index(1) {|g, i| puts "#{i}. #{g.name}"}
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.chomp
+    if artist = Artist.find_by_name(input)
+      sorted_songs = artist.songs.sort_by {|s| s.name}
+      sorted_songs.each.with_index(1) {|s, i| puts "#{i}. #{s.name} - #{s.genre.name}"}
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.chomp
+    if genre = Genre.find_by_name(input)
+      sorted_songs = genre.songs.sort_by {|s| s.name}
+      sorted_songs.each.with_index(1) {|s, i| puts "#{i}. #{s.artist.name} - #{s.name}"}
     end
   end
 end
