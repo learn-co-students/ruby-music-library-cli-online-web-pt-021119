59b3ca6bb994c2dd1d99b66798f53d5b23e3c1a4
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-04-02 01:47:17 +0000

    updated list methods

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,46 +9,63 @@ class MusicLibraryController
 
   def call
     input = ""
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    input = gets.chomp!
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      
+      input = gets.strip
+    end
   end
 
   def list_songs
-    songs = music.collect {|x| x.chomp(".mp3")}
-    songs.collect! {|x| x.split(" - ")}
-    songs_hash = songs.collect do |x|
-      {:artist => x[0], :song => x[1], :genre => x[2]}
-    end
-    songs_hash.sort_by! {|hash| hash[:song]}
-    songs_sorted = songs_hash.collect do |x|
-      [x[:artist], x[:song], x[:genre]]
-    end
-    songs_sorted.each_with_index do |x, index|
-      puts "#{index+1}. #{x.join(" - ")}"
+    Song.all.sort_by(&:name).each_with_index do |x, index|
+      puts "#{index+1}. #{x.artist.name} - #{x.name} - #{x.genre.name}"
     end
   end
 
   def list_artists
-    artists = music.collect {|x| x.chomp(".mp3")}
-    artists.collect! {|x| x.split(" - ")}
-    artists_hash = artists.collect do |x|
-      {:artist => x[0], :song => x[1], :genre => x[2]}
+    Artist.all.sort_by(&:name).each_with_index do |x, index|
+      puts "#{index+1}. #{x.name}"
     end
-    artists_hash.sort_by! {|hash| hash[:artist]}
-    artists_sorted = artists_hash.collect do |x|
-      [x[:artist], x[:song], x[:genre]]
+  end
+
+  def list_genres
+    Genre.all.sort_by(&:name).each_with_index do |x, index|
+      puts "#{index+1}. #{x.name}"
     end
-    artist_array = artists_sorted.collect {|x| x[0]}.uniq!
-    artist_array.each_with_index do |x, index|
-      puts "#{index+1}. #{x}"
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    artist = gets.strip
+    songs = music.collect {|x| x.chomp(".mp3")}
+    songs.collect! {|x| x.split(" - ")}.sort_by! {|x| x[1]}
+    songs.select! {|x| x if x[0] == artist}
+    songs.each {|x| x.delete_at(0)}
+    songs.each_with_index do |x, index|
+      puts "#{index+1}. #{x[0]} - #{x[1]}"
+    end
+    #binding.pry
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    genre = gets.chomp
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    song_num = gets.chomp
+    #binding.pry
+    if (1..list_songs.length).include?(song_num)
+      #play song
     end
   end
 
