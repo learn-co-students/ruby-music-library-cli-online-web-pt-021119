d97fa97f866d69ee06bab73bedc8521cd0e00017
michael white <michaeljwhite@gmail.com>
2019-04-04 14:17:43 -0400

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -3,8 +3,7 @@ class MusicLibraryController
 
 	def initialize(path = './db/mp3s')
 		@path = path
-		music_importer = MusicImporter.new(@path)
-		music_importer.import
+		MusicImporter.new(@path).import
 	end
 
 	def call
