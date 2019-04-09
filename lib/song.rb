eea614fe2c40957697ccf38b0a9837704291c9fd
Malcolm Leake <echelon1089@gmail.com>
2019-04-09 04:44:46 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class Song 
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name 
+   
+  end
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.destroy_all 
+  @@all.clear
+  end
+  
+  def save
+   @@all << self
+  end
+  
+  def self.create(name)
+    new(name).save.last
+  end
+  
+end
