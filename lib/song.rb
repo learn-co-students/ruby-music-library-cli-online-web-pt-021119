08e37e1844550159b269c232082ec0e2a1469613
areid987 <read.musik@gmail.com>
2019-04-09 21:28:59 -0500

    reclaim the progress

--- lib/song.rb
+++ lib/song.rb
@@ -1,45 +1,65 @@
 class Song
-
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name
+  attr_reader :artist, :genre
 
   @@all = []
 
-  def initialize (name, artist=nil, genre=nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def artist=(artist)
     @artist = artist
-    self.genre= (genre) unless genre.nil?
+    artist.add_song(self)
   end
 
   def genre=(genre)
-    @genre = genre #=> instance of Genre genre.songs (array)
+    @genre = genre
     genre.songs << self unless genre.songs.include?(self)
-    genre
   end
-
 
   def self.all
     @@all
   end
 
   def self.destroy_all
-    @@all.clear
+    all.clear
   end
 
   def save
-    @@all << self
+    self.class.all << self
   end
 
   def self.create(name)
-    song = Song.new(name)
+    song = new(name)
     song.save
     song
+
+    # Or, as a one-liner:
+    # new(name).tap{ |s| s.save }
   end
 
   def self.find_by_name(name)
-    @@all.find {|song| song.name == name}
+    all.detect{ |s| s.name == name }
   end
 
   def self.find_or_create_by_name(name)
-     self.find_by_name(name) || self.create(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |s| s.save }
   end
 end
