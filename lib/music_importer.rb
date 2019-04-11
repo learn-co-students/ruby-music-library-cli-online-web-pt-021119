afe5d3c242fd9d0ca2c03b50abaf0c81725c6720
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-11 02:19:59 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,9 +1,21 @@
+require "pry"
+
 class MusicImporter
+
   attr_reader :path
 
   def initialize(path)
     @path = path
   end
 
+  def files
+    Dir.entries(path).select {|f| !File.directory? f}
+  end
+
+  def import
+    self.files.each do |filename|
+     Song.create_from_filename(filename)
+   end
+  end
 
 end
