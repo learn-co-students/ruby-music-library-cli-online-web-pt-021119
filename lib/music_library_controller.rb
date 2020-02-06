5c5c64031e31724bddca27d2cc36c041f66163ab
github username <github email address>
2020-02-05 18:43:58 -0700

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,7 +20,24 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      input = gets.chomp
+   
+
+    input = gets.chomp
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
   end
 
@@ -53,5 +70,24 @@ class MusicLibraryController
         puts "#{i}. #{song.name} - #{song.genre.name}"
       end
     end
+
+    def list_songs_by_genre
+      puts "Please enter the name of a genre:"
+      input = gets.chomp
+      if genre = Genre.find_by_name(input)
+        sorted_songs = genre.songs.sort_by {|s| s.name}
+        sorted_songs.each.with_index(1) {|s, i| puts "#{i}. #{s.artist.name} - #{s.name}"}
+      end
+    end
+
+    def play_song
+      puts "Which song number would you like to play?"
+      input = gets.chomp.to_i
+      if input >= 1 && input <= Song.all.size
+        selection = Song.all.sort_by {|s| s.name}[input -= 1]
+        puts "Playing #{selection.name} by #{selection.artist.name}" if selection
+      end
+    end
   end
-end
+end 
+ 
